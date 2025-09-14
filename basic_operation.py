660d3075206acc3b21c0bdac7d1a8ae3411f7936
Charwekey <crabisabutey@gmail.com>
2025-09-14 21:24:27 +0000

    added my first python assignment

---
+++ basic_operation.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+product_result = number1 * number2  
+print("The product of", number1, "and", number2, "is:", product_result)
+
+difference_result = number1 - number2  
+print("The difference when", number2, "is subtracted from", number1, "is:", difference_result)
+
+sum_result = number1 + number2
+print("The sum of", number1, "and", number2, "is:", sum_result)
